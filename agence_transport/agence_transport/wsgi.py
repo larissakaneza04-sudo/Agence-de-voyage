6f1e00b3ef906134e72ce0719232fd32a9f724a2
GRIZZLY <larissakaneza04@gmail.com>
2025-12-12 20:26:42 +0200

    add reservations as app

--- agence_transport/agence_transport/wsgi.py
+++ agence_transport/agence_transport/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
+import sys
+# Add the project root to the Python path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agence_transport.settings')
-
 application = get_wsgi_application()
